68f50c5fd063eee067d86367b73c27e77478049d
Helar Jaadla <helar.jaadla@eesti.ee>
2020-03-17 00:39:25 +0200

    Server/Client Maven struktuur

---
+++ client/src/main/java/ee/ut/oop/App.java
@@ -0,0 +1,13 @@
+package ee.ut.oop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ server/src/main/java/ee/ut/oop/Server.java
@@ -0,0 +1,13 @@
+package ee.ut.oop;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
